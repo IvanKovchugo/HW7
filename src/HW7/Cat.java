bdd52c1c52c9d1ade2ac3a39b1b198b4f37f9293
IvanKovchugo <91083296+IvanKovchugo@users.noreply.github.com>
2021-10-12 22:39:31 +0300

    HW7

---
+++ src/HW7/Cat.java
@@ -0,0 +1,42 @@
+package HW7;
+
+public class Cat {
+    private String name;
+    private int appetite;
+    private boolean isHungry;
+
+    public Cat(String name) {
+        this.name = name;
+        this.appetite = 5;
+        this.isHungry = true;
+    }
+
+    public void setHungry(boolean hungry) {
+        isHungry = hungry;
+    }
+
+    public int getAppetite() {
+        return appetite;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void info() {
+        System.out.println("Name: " + name + ", hungry: " + isHungry);
+    }
+
+
+
+    public void eat(Plate plate) {
+        if (plate.getFood() >= appetite) {
+            plate.decreaseFood(appetite);
+            isHungry = false;
+            System.out.println(name + " поел из тарелки");
+        }
+        if (plate.getFood() < appetite) {
+            isHungry = true;
+        }
+    }
+}
--- src/HW7/Main.java
+++ src/HW7/Main.java
@@ -3,6 +3,34 @@ package HW7;
 public class Main {
 
     public static void main(String[] args) {
-	
+
+        Cat[] cat = new Cat[]{new Cat("Barsik"), new Cat(" Tolik "), new Cat("Murzik")};
+        Plate plate = new Plate(150);
+
+        plate.info();
+        cat[0].info();
+        plate.info();
+        cat[1].info();
+        plate.info();
+        cat[2].info();
+
+        while (true) {
+            for (int i = 0; i < 3; i++) {
+                if (plate.getFood() >= cat[i].getAppetite()) {
+                    cat[i].eat(plate);
+
+                    plate.info();
+                    cat[i].info();
+
+                }
+                if (plate.getFood() < cat[i].getAppetite()) {
+                    System.out.println("Еды недостаточно!");
+                    plate.addFood(50);
+                    plate.info();
+                    return;
+                }
+            }
+        }
+
     }
 }
